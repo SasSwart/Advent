7bed1148c1803b036d69c6ee6e5eec2f3f5a352c
Sas Swart <sasswartcdk@protonmail.com>
2021-12-16 19:33:05 +0200

    2021 day 3 1

---
+++ 2021/3/1.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func input() [][]byte {
+	file, _ := os.ReadFile("./input")
+	return bytes.Split(file, []byte("\n"))
+}
+
+func main() {
+	input := input()
+
+	lineLength := len(input[0])
+	var bitFrequency []uint = make([]uint, lineLength)
+	var gamma, epsilon, mask uint = 0, 0, (1<<lineLength - 1)
+
+	for _, bits := range input {
+		for i := 0; i < lineLength; i++ {
+			bitFrequency[i] += uint(bits[i] - 48)
+		}
+	}
+
+	for i := 0; i < len(bitFrequency); i++ {
+		gamma = gamma << 1
+		epsilon = epsilon << 1
+		gamma += (bitFrequency[i] << 1) / uint(len(input))
+	}
+
+	epsilon = mask &^ gamma
+	fmt.Printf("%b\n", gamma)
+	fmt.Printf("%b\n", epsilon)
+	fmt.Printf("%v\n", gamma*epsilon)
+}
