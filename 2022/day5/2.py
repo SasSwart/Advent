80ce8e22892df8c69dff9d2c3e5583613fb45b6e
Sas Swart <sas.swart@xneelo.com>
2022-12-06 15:45:20 +0200

    day5

---
+++ 2022/day5/2.py
@@ -0,0 +1,24 @@
+import re
+
+f = open("test.txt", "r")
+board, instructions = map(lambda buf : buf.split("\n"), f.read().split("\n\n"))
+
+board_height = len(board)-1
+num_stacks = (len(board[-1])+1)//4
+stacks = []
+
+
+for i in range(num_stacks):
+    stack = []
+    for line in range(board_height-1, -1, -1):
+        crate = board[line][4*i+1]
+        if crate != " ":
+            stack.append(crate)
+    stacks.append(stack)
+
+for instruction in instructions:
+    num_crates, from_col, to_col = map(int, re.findall("\d+", instruction))
+    stacks[to_col-1].extend(stacks[from_col-1][-num_crates:])
+    stacks[from_col-1] = stacks[from_col-1][:-num_crates]
+
+print([s[-1] for s in stacks])
